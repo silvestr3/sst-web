518fec37ed028023f94cda6ccdf963ad6b9ca772
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-19 18:58:50 -0300

    feat: add toaster

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,9 +4,7 @@ const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.string().url(),
 });
 
-const parsedEnv = envSchema.safeParse({
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-});
+const parsedEnv = envSchema.safeParse(process.env);
 
 if (parsedEnv.success === false) {
   throw new Error(
