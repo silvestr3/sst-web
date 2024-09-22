c485e3f61276a8e90c3244d8c4fa367e15af3796
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-22 17:51:10 -0300

    feat: Add access token to backend requests

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,29 @@
 import { env } from "@/env";
+import { getServerSession } from "next-auth";
+import { options } from "./auth";
+import { redirect } from "next/navigation";
 
-export function api(path: string, init?: RequestInit) {
+export async function api(path: string, init?: RequestInit) {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/login?expired");
+  }
+
   const baseUrl = env.NEXT_PUBLIC_API_URL;
-
   const url = new URL(path, baseUrl);
 
-  return fetch(url, init);
+  const request = fetch(url, {
+    ...init,
+    headers: {
+      ...init?.headers,
+      Authorization: `Bearer ${session.user.accessToken}`,
+    },
+  });
+
+  if ((await request).status === 401) {
+    redirect("/login?expired");
+  }
+
+  return request;
 }
