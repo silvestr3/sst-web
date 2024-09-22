3031b6c872eeb7effabdbc3e3b24a1b617f9ec06
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-22 18:32:39 -0300

    feat: Add loader between pages

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,6 +4,7 @@ import { options } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
+import NextTopLoader from "nextjs-toploader";
 
 interface LayoutProps {
   children: ReactNode;
@@ -19,6 +20,12 @@ export default async function AppLayout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col antialiased">
       <Header />
+      <NextTopLoader
+        easing="ease"
+        speed={200}
+        crawlSpeed={200}
+        color="#c9c9e4"
+      />
       <div className="flex flex-1">
         <Sidebar />
         <main className="flex-1 p-8">{children}</main>
