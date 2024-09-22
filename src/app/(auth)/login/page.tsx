84cd5d1d27fee39890a61b6717705d589c1b4c2c
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-22 11:30:50 -0300

    feat: Setup useForm on login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,21 +1,48 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type LoginType = z.infer<typeof loginSchema>;
 
 export default function LoginPage() {
+  const { register, handleSubmit } = useForm<LoginType>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  async function handleUserLogin(data: LoginType) {
+    console.log(data);
+  }
+
   return (
-    <form className="flex flex-col gap-2 items-center justify-center">
+    <form
+      onSubmit={handleSubmit(handleUserLogin)}
+      className="flex flex-col gap-2 items-center justify-center"
+    >
       <h1 className="font-bold text-2xl mb-6">Entre em sua conta</h1>
 
       <div className="flex flex-col gap-2">
         <Label>E-mail</Label>
-        <Input type="email" className="w-[320px]" />
+        <Input {...register("email")} type="email" className="w-[320px]" />
       </div>
 
       <div className="flex flex-col gap-2">
         <Label>Senha</Label>
-        <Input type="password" className="w-[320px]" />
+        <Input
+          {...register("password")}
+          type="password"
+          className="w-[320px]"
+        />
       </div>
 
       <div className="flex flex-col gap-4 mt-8 items-center justify-center w-1/2">
